e0868c61ac4c1889c7df3e721a5c4e56ba4b966c
neopha <haupt.konstantin@web.de>
2011-12-03 15:32:52 +0100

    commit first test-class to repo

---
+++ src/main/java/de/gmx/kansil/cortex/luciditylog/LucidityLogView.java
@@ -0,0 +1,17 @@
+package de.gmx.kansil.cortex.luciditylog;
+
+import javax.swing.JFrame;
+
+public class LucidityLogView extends JFrame {
+
+	private static final long serialVersionUID = 2248216024060304629L;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
